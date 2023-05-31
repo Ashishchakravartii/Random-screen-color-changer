a74734941dd045e9438161b28f3eecabb98b7410
Aditya kumar <Adityakumar14022002@gmail.com>
2023-05-31 12:45:52 +0530

    Second Method

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ btn.addEventListener("click", () => {
     }
 
 })
+
+// Second method
+
+// function randomRGB() {
+//     var x = Math.floor(Math.random() * 256);
+//     var y = Math.floor(Math.random() * 256);
+//     var z = Math.floor(Math.random() * 256);
+//     var Color = "rgb(" + x + "," + y + "," + z + ")";  
+//     document.body.style.backgroundColor = Color;
+//   }
+//   randomRGB();
+  
